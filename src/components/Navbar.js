f4898efe778100aad8dec89ffbc12973ac9de9bc
Waseem k <you@example.com>
2023-08-31 23:24:18 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Navbar(props) {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+               <a style={{fontSize:"25px"}} href="/"><strong>Texto</strong></a>
+               
+                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                    </div>
+               
+            </div>
+        </nav>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+    title: 'Set title here',
+    aboutText: 'About'
+  };
